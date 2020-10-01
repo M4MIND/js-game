e9ee5d4229a10f39709a0a1fe2ffce6b1301edae
Egor Sysoev <m4mindscape@gmail.com>
2020-10-01 23:54:22 +0500

    Create project

---
+++ source/js/game/arm/pistol.js
@@ -0,0 +1,22 @@
+var pistol = function () {
+    return {
+        maxBullet: 7,
+        bullet: 7,
+        allBullet: 99,
+        lastFire: Date.now(),
+        timeOut: 60,
+        fire: function (pos, angle) {
+            if (Date.now() - this.lastFire > this.timeOut) {
+                this.lastFire = Date.now();
+                this.bullet -= 1;
+                main.pushObject(bullet(pos, angle))
+            }
+        },
+        reload: function () {
+            if (this.bullet !== this.maxBullet) {
+                this.bullet = this.allBullet >= this.maxBullet ? this.maxBullet : this.allBullet;
+                this.allBullet -= this.bullet;
+            }
+        }
+    }
+}
